456173e54be7f37ca1ef97868d1b78e111a2745e
nates linux desktop <nhapeman@ai.co>
2017-12-12 23:03:07 -0800

    need to figure out whats wrong with the pixelate algo now.

--- src/imageFilter.js
+++ src/imageFilter.js
@@ -26,33 +26,99 @@ class ImageFilter
         var xIter = 0;
         var yIter = 0;
         var blockIndex = 0;
-        var startingIndex = 0;
-        var colorSum = 0;
+        var blockXY = 0;
+        var blockX = 0;
+        var blockY = 0;
+        var startingX = 0;
+        var startingY = 0;
+        var colorSum = {"r": 0, "g": 0, "b": 0 };
+        var averageColor = {"r": 0, "g": 0, "b": 0 };
         var colorAverage = 0;
+        var pixelIndex = 0;
+        var colors = 0;
 
         for(xIter = 0; xIter < xItersMax; ++xIter)
         {
             for(yIter = 0; yIter < yItersMax; ++yIter)
             {
                 // Calculate average pixel color for this block.
-                colorSum = 0;
-                //startingIndex = xIter * yIter  ??
+                colorSum = {"r": 0, "g": 0, "b": 0 };
+                averageColor = {"r": 0, "g": 0, "b": 0 };
 
+                startingX = xIter * scale;
+                startingY = yIter * scale;
 
                 // for loop for getting color sum of all pixels for block.
                 for(blockIndex = 0; blockIndex < numPixelPerBlock; ++blockIndex)
                 {
                     // translate index to x, y coordinates.
+                    blockXY = this.indexToXY(blockIndex, scale);
+                    blockX = startingX + blockXY.x;
+                    blockY = startingY + blockXY.y;
 
+                    pixelIndex = this.xyToIndex(blockX, blockY, width, height);
+                    colors = this.getColorsAtIndex(imageData, pixelIndex);
+
+                    colorSum.r += colors.r;
+                    colorSum.g += colors.g;
+                    colorSum.b += colors.b;
                 }
 
-                colorAverage = colorSum / numPixelPerBlock;
+                averageColor =
+                {
+                    "r": colorSum.r/numPixelPerBlock,
+                    "g": colorSum.g/numPixelPerBlock,
+                    "b": colorSum.b/numPixelPerBlock,
+                };
 
-                // for loop for getting color sum of all pixels for block.
+                //console.log(averageColor);
 
+                // Set Colors for block to average color.
+                for(blockIndex = 0; blockIndex < numPixelPerBlock; ++blockIndex)
+                {
+                    // translate index to x, y coordinates.
+                    blockXY = this.indexToXY(blockIndex, width);
+                    blockX = startingX + blockXY.x;
+                    blockY = startingY + blockXY.y;
+
+                    pixelIndex = this.xyToIndex(blockX, blockY, width, height);
+                    this.setColorsAtIndex(imageData, pixelIndex, averageColor);
+                }
             }
         }
 
+    }
+
+    getColorsAtIndex(imageData, index)
+    {
+        var realIndex = index*4;
+        var r = imageData.data[realIndex + 0];
+        var g = imageData.data[realIndex + 1];
+        var b = imageData.data[realIndex + 2];
+        return {"r": r, "g": g, "b": b };
+    }
+
+    setColorsAtIndex(imageData, index, colors)
+    {
+        var realIndex = index*4;
+        imageData.data[realIndex + 0] = colors.r;
+        imageData.data[realIndex + 1] = colors.g;
+        imageData.data[realIndex + 2] = colors.b;
+    }
+
+    indexToXY(index, width)
+    {
+        return {
+            "x": index % width,
+            "y": Math.floor(index / width)
+        };
+    }
+
+    xyToIndex(x, y, width, height)
+    {
+        if(x < 0 || y < 0 || x >= width || y >= height)
+            return -1;
+        return x + y * width;
     }
 
     grayScale(imageData)
